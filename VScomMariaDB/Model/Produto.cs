629b22b579972330bf2ef60a1261dfd06923602b
MillerMagnus <millermagnus@outlook.com>
2024-02-15 22:27:51 -0300

    update

---
+++ VScomMariaDB/Model/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VScomMariaDB.Model
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        [MaxLength(150)]
+        public string? Descricao { get; set; }
+        
+        [MaxLength(150)]
+        public string? Preco { get; set; }
+
+        [MaxLength(15)]
+        public string? Disponivel { get; set; }
+
+        [MaxLength(15)]
+        public string? Novidade { get; set; }
+    }
+}
